54d88c8ad063c6309055e5d75229f57932b65460
HenrikGharagyozyan1 <henrik.gharagyozyan1@email.com>
2026-01-31 21:40:55 +0400

    Refactor Planet into visual-only component

--- src/scene/Planet.cpp
+++ src/scene/Planet.cpp
@@ -1,26 +1,11 @@
 #include "scene/Planet.hpp"
-#include <cmath>
 
-namespace SolarSystem2D 
+namespace SolarSystem2D
 {
 
-    Planet::Planet(Entity& entity, float orbitRadius, float orbitSpeed, float size)
-        : m_Entity(entity)
-        , m_OrbitRadius(orbitRadius)
-        , m_OrbitSpeed(orbitSpeed)
+    Planet::Planet(Entity& entity, float size)
     {
-        m_Entity.getTransform().setScale({ size, size, 1.0f });
+        entity.getTransform().setScale({ size, size, 1.0f });
     }
 
-    void Planet::update(float deltaTime)
-    {
-        m_Angle += m_OrbitSpeed * deltaTime;
-
-        m_Entity.getTransform().setPosition({
-            std::cos(m_Angle) * m_OrbitRadius,
-            std::sin(m_Angle) * m_OrbitRadius,
-            0.0f
-        });
-    }
-
-} 
+}
--- src/scene/Planet.hpp
+++ src/scene/Planet.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Entity.hpp"
+#include "scene/Entity.hpp"
 
 namespace SolarSystem2D
 {
@@ -8,16 +8,7 @@ namespace SolarSystem2D
     class Planet
     {
     public:
-        Planet(Entity& entity, float orbitRadius, float orbitSpeed, float size);
-
-        void update(float deltaTime);
-
-    private:
-        Entity& m_Entity;
-
-        float m_OrbitRadius;
-        float m_OrbitSpeed; // radians per second
-        float m_Angle = 0.0f; // current angle in orbit
+        Planet(Entity& entity, float size);
     };
 
 }
